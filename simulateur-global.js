14dd2a64fbd068d6af9d5e7d0b5219964ab21c69
CreaSoley <artcreatifmeg@gmail.com>
2026-01-23 09:57:35 +0100

    Update simulateur-global.js

--- simulateur-global.js
+++ simulateur-global.js
@@ -128,9 +128,12 @@ UV2.start(interval, (text) => {
   return new Promise(resolve => {
     const u = new SpeechSynthesisUtterance(text);
     u.lang = "ja-JP";
+    u.rate = 0.95;
     u.onend = resolve;
     speechSynthesis.speak(u);
   });
+});
+
 });
 
    }
